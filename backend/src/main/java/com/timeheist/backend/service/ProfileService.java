233634340a8ca2d0685682a076bc62f0d638fcc5
Akash <akasharade8@gmail.com>
2026-08-15 13:13:27 +0530

    added player profile backend

---
+++ backend/src/main/java/com/timeheist/backend/service/ProfileService.java
@@ -0,0 +1,60 @@
+package com.timeheist.backend.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import com.timeheist.backend.entity.PlayerProfile;
+import com.timeheist.backend.exception.ResourceNotFoundException;
+import com.timeheist.backend.repository.ProfileRepo;
+
+@Service
+public class ProfileService {
+
+    @Autowired
+    private ProfileRepo profileRepo;
+
+    public PlayerProfile createPlayer(PlayerProfile player) {
+        try {
+            return profileRepo.save(player);
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to create player", e);
+        }
+    }
+
+    public PlayerProfile updatePlayer(PlayerProfile player) {
+        Long playerId = player.getId();
+
+        if (!profileRepo.existsById(playerId)) {
+            throw new ResourceNotFoundException(
+                    "Player with ID " + playerId + " does not exist."
+            );
+        }
+
+        try {
+            return profileRepo.save(player);
+        } catch (Exception e) {
+            throw new RuntimeException(
+                    "Failed to update player with ID " + playerId, e
+            );
+        }
+    
+    }
+
+    public void deletePlayer(Long playerId) {
+        if (!profileRepo.existsById(playerId)) {
+            throw new ResourceNotFoundException(
+                    "Player with ID " + playerId + " does not exist."
+            );
+        }
+
+        try {
+            profileRepo.deleteById(playerId);
+        } catch (Exception e) {
+            throw new RuntimeException(
+                    "Failed to delete player with ID " + playerId, e
+            );
+        }
+    }
+    
+}
