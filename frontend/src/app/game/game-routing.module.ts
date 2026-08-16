5f9e0430484cd76e3d9752abfeaf5b4be469d692
Mohammed-mehdi-raza <mehdirazajafri2040@gmail.com>
2026-08-16 15:36:48 +0530

    created map,diamond and exit component

--- frontend/src/app/game/game-routing.module.ts
+++ frontend/src/app/game/game-routing.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { GameScreenComponent } from './components/game-screen/game-screen.component';
+import { CommonModule } from '@angular/common';
 
 const routes: Routes = [
   {path:'',component:GameScreenComponent}
 ];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
+  imports: [CommonModule, RouterModule.forChild(routes), GameScreenComponent],
   exports: [RouterModule]
 })
 export class GameRoutingModule { }
