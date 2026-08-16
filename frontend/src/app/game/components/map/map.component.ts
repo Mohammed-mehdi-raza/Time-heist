4e88e661090f86f676d59145cf873825f1fb6778
Mohammed-mehdi-raza <mehdirazajafri2040@gmail.com>
2026-08-16 12:09:52 +0530

    updated map component

--- frontend/src/app/game/components/map/map.component.ts
+++ frontend/src/app/game/components/map/map.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, Input, NgModule } from '@angular/core';
+import { GameMap, TileType } from '../../models/map.model';
+import { PlayerComponent } from '../player/player.component';
+import { DiamondComponent } from '../diamond/diamond.component';
+import { ExitComponent } from '../exit/exit.component';
+import { GuardComponent } from '../guard/guard.component';
+import { CommonModule, NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-map',
   standalone: true,
-  imports: [],
+  imports: [PlayerComponent, DiamondComponent, ExitComponent, GuardComponent,NgClass,CommonModule],
   templateUrl: './map.component.html',
   styleUrl: './map.component.scss'
 })
 export class MapComponent {
+  @Input({ required: true }) gameMap!: GameMap;
 
+  getTileClass(tile: TileType): string {
+    return `tile-${tile}`;
+  }
 }
