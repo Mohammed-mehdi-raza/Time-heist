c376b45ad80ca8826faf11db2c1d934413886b27
Mohammed-mehdi-raza <mehdirazajafri2040@gmail.com>
2026-08-17 08:57:30 +0530

    created how to play page

--- frontend/src/app/auth/auth-routing.module.ts
+++ frontend/src/app/auth/auth-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthParentComponent } from './auth-parent/auth-parent.component';
+import { HowToPlayComponent } from './how-to-play/how-to-play.component';
 
 const routes: Routes = [
-  { path: '', component: AuthParentComponent }
+  { path: '', component: AuthParentComponent },
+  { path: 'howToPlay', component: HowToPlayComponent }
 ];
 
 @NgModule({
